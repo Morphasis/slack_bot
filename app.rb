fe956b16f1533a7cb6a5b69085a0503d69dd7b73
Jack Sharville <jacksharville@gmail.com>
2016-09-18 10:32:14 +0100

    some buggy but intesting commit data requires more logic to be written but currently out for luch :P

--- app.rb
+++ app.rb
@@ -8,15 +8,25 @@ post '/gateway' do
 
   action, repo = message.split('_').map {|c| c.strip.downcase }
   repo_url = "https://api.github.com/repos/#{repo}"
+  activity_url = "https://api.github.com/users/#{repo}/events"
 
   case action
     when 'issues'
       resp = HTTParty.get(repo_url)
-      puts resp
       resp = JSON.parse resp.body
+      puts resp
       respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
     when 'help'
-      respond_message "Hi there my name is Kong Bot. I'm here to provide interesting and new bot functionality :bowtie: currently supported commands are issues_(repo) to get number of PR's"
+      respond_message "Hi there my name is Kong Bot. I'm here to provide
+      interesting and new bot functionality :bowtie: currently supported
+       commands are issues_(repo) to get number of PR's, help and activity_(person)"
+    when 'activity'
+      resp = HTTParty.get(activity_url)
+      resp = JSON.parse resp.body
+      respond_message "Three most recent commits (may not be work related) (currently in progress as it works per push requires more logic :robot_face:)
+      #{resp[0]["payload"]["commits"][0]["message"]}
+      #{resp[0]["payload"]["commits"][1]["message"]}
+      #{resp[0]["payload"]["commits"][2]["message"]}"
   end
 end
 
