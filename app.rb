a305ec86a2d92f6c447d7dad9a645611ceeef1d4
Jack Sharville <jacksharville@gmail.com>
2016-09-19 13:12:11 +0100

    need params log temp

--- app.rb
+++ app.rb
@@ -3,17 +3,27 @@ require 'httparty'
 require 'json'
 
 post '/gateway' do
+  puts "PARAMS: #{params.inspect}"
   message = params[:text].gsub(params[:trigger_word], '').strip
 
   action, repo = message.split('_').map {|c| c.strip.downcase }
   repo_url = "https://api.github.com/repos/#{repo}"
   activity_url = "https://api.github.com/users/#{repo}/events"
+  # issues_url = "https://api.github.com/repos/#{repo}/issues/events"
 
   case action
+  # when 'issuesDetailed'
+  #   resp = HTTParty.get(repo_url)
+  #   resp = JSON.parse resp.body
+  #   resp_issue = HTTParty.get(issues_url)
+  #   resp_issue = JSON.parse resp_issue.body
+  #   resp_issue.each { |x|
+  #     puts x["id"]
+  #   }
+  #   respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
     when 'issues'
       resp = HTTParty.get(repo_url)
       resp = JSON.parse resp.body
-      puts resp
       respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
     when 'help'
       respond_message "Hi there my name is Kong Bot. I'm here to provide
